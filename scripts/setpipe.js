06fd5680f0ae87ff7f13432ffb6861c134f57e50
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-20 01:03:17 +1100

    minor cleanup and print debug stuff for maybe final fix

--- scripts/setpipe.js
+++ scripts/setpipe.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 
 module.exports.default = function ({
   dir = "../pipe",
-  dir2 = "unset",
   version = "",
   branch = "none",
   tag = "none",
@@ -11,9 +10,9 @@ module.exports.default = function ({
 } = {}) {
   // const raw = fs.readFileSync("./package.json");
   // const pkg = JSON.parse(raw);
-  fs.writeFileSync(`${dir2}/version`, version);
-  if (branch !== "none") fs.writeFileSync(`${dir2}/branch`, branch);
-  if (tag !== "none") fs.writeFileSync(`${dir2}/tag`, tag);
-  if (channel !== "none") fs.writeFileSync(`${dir2}/channel`, channel);
+  fs.writeFileSync(`${dir}/version`, version);
+  if (branch !== "none") fs.writeFileSync(`${dir}/branch`, branch);
+  if (tag !== "none") fs.writeFileSync(`${dir}/tag`, tag);
+  if (channel !== "none") fs.writeFileSync(`${dir}/channel`, channel);
   return 0;
 };
