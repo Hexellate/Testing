afed7eb9534a982bf8bebc18de94f6ff6129df45
Daniel Nichols <pnupy9@gmail.com>
2018-11-19 18:19:11 +1100

    AAAAAAAAAAAAAHHHHHRHRGRG
    AAAAAAAAAAAAAHHHHHRHRGRGRGRRRGURBHJB EORWPSOJWPJORGWOIRGWSGODEWPGOHEPW09GJEDPOKSD!!!!!!!!!!!!!!!0924QU8T63095JRGHWPE09UJ0PWHRGW

--- scripts/getVer.js
+++ scripts/getVer.js
@@ -11,6 +11,7 @@ module.exports.default = function (comp) {
   const patch = semver.patch(pkg.version);
   let channel = "stable";
   let prever = "";
+  console.log(semver.prerelease(pkg.version));
   if (semver.prerelease(pkg.version) != null) {
     [channel, prever] = semver.prerelease(pkg.version);
   }
