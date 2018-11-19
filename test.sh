6980995ec035a84cb78e68a0d9f5769c75fc8042
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-19 23:02:58 +1100

    Hopefully final fix to build script

--- test.sh
+++ test.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 #pkgchannel="stable"
-pkgchannel="dankmeme"
-channel="stable"
-branch="develop/test"
-#branch="master"
-#BUILD_REASON="PullRequest"
-BUILD_REASON="CI"
-# echo pkgchannel
-# echo channel
-# echo branch
-# echo BUILD_REASON
-if [[ ("${pkgchannel}" != "${channel}" && "${BUILD_REASON}" != "PullRequest") &&  !(${branch} =~ ^hotfix/.* || ${branch} =~ ^feature/.*)  ]]
+# pkgchannel="dankmeme"
+# channel="stable"
+# branch="develop/test"
+# #branch="master"
+# #BUILD_REASON="PullRequest"
+# BUILD_REASON="CI"
+# # echo pkgchannel
+# # echo channel
+# # echo branch
+# # echo BUILD_REASON
+# if [[ ("${pkgchannel}" != "${channel}" && "${BUILD_REASON}" != "PullRequest") &&  !(${branch} =~ ^hotfix/.* || ${branch} =~ ^feature/.*)  ]]
+#   then
+#     echo yay
+#   else
+#     echo nay
+# fi
+AGENT_OS="Windows_NT"
+
+if [[ "${AGENT_OS}" == "Darwin" ]]
   then
-    echo yay
-  else
-    echo nay
+    OS="mac"
+  elif [[ "${AGENT_OS}" == "Windows_NT" ]]
+  then
+    OS="win"
+  elif [[ "${AGENT_OS}" == "Linux" ]]
+  then
+    OS="lin"
 fi
+echo $(OS)
+echo ${OS}
