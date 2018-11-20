042d2b433e7f5d33f8e6b8adb082bd1d146d5c2f
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-20 14:43:11 +1100

    Some script changes

--- scripts/transientbumpver.sh
+++ scripts/transientbumpver.sh
@@ -5,23 +5,24 @@
 # bump version on hotfix pr, hotfix commit, or merge to master from hotfix
 # merge to master should update version and tag in release pipeline
 # All releases should tag
-
-if [[ [["$(channel)" == "stable"]] && ! [[ "$(branch)" == "master" && ! [["Buildmessage" =~ "^(Merge pull request #\d{1,4} from .*\/hotfix\/.*)|(Merge branch 'hotfix\/.*')$"]] ]] # TODO: If building in main, then it must be a pull or merge from hotfix
+printenv
+if [[ (${CHANNEL} == "stable") && ("Buildmessage" =~ "^(Merge pull request #\d{1,4} from .*\/hotfix\/.*)|(Merge branch 'hotfix\/.*')$") ]]
+# if [[ ("${pkgchannel}" != "${CHANNEL}" && "${BUILD_REASON}" != "PullRequest") &&  !(${branch} =~ ^hotfix/.* || ${branch} =~ ^feature/.*)  ]]
 then
   node -e "require('./scripts/bumpver.js').default({'channel':'$(channel)','bump':'patch'})"
 
   newver="$(node -p 'require("./scripts/getver.js").default("full")')"
 fi
 
-if [[ "$(channel)" == "next" ]] || [[ "$(channel)" == "canary" ]]
+if [[ ("${CHANNEL}" == "next") || ("${CHANNEL}" == "canary") ]]
 then
   node -e "require('./scripts/setver.js').default({'channel':$(channel),'comp':'pre', 'val':$(Build.BuildId)})"
 
-  newver="$(node -p 'require("./scripts/getver.js").default("full")')"
+  # newver="$(node -p 'require("./scripts/getver.js").default("full")')"
 fi
 
-tag="v$(newver)"
-version = "$(newver)"
+# tag="v$(newver)"
+# version = "$(newver)"
 
 # exports: tag, version
 # write new version to pipe
