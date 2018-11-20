68b10a1e0a40a326006061da2a5f85034658c3e3
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-20 16:58:54 +1100

    Cleanup

--- scripts/transientbumpver.sh
+++ scripts/transientbumpver.sh
@@ -9,7 +9,7 @@ printenv
 echo Current version: $(node -p 'require("./scripts/getver.js").default("full")')
 if [[ ("${CHANNEL}" == "stable") && ("${BUILD_SOURCEVERSIONMESSAGE}" =~ ^(Merge pull request \#[0-9]{1,4} from .*\/hotfix\/.*)|(Merge branch \'hotfix\/.*\')$) ]]
 then
-  node -e "require('./scripts/bumpver.js').default({'channel':'$(channel)','bump':'patch'})"
+  node -e "require('./scripts/bumpver.js').default({'channel':'${CHANNEL}','bump':'patch'})"
 
   #newver="$(node -p 'require("./scripts/getver.js").default("full")')"
   echo Bump patch to: $(node -p 'require("./scripts/getver.js").default("full")')
@@ -17,7 +17,7 @@ fi
 
 if [[ ("${CHANNEL}" == "next") || ("${CHANNEL}" == "canary") ]]
 then
-  node -e "require('./scripts/setver.js').default({'channel':$(channel),'comp':'pre', 'val':$(Build.BuildId)})"
+  node -e "require('./scripts/setver.js').default({'channel':'${CHANNEL}','comp':'pre', 'val':$(Build.BuildId)})"
 
   #newver="$(node -p 'require("./scripts/getver.js").default("full")')"
   echo Set prerelease info: $(node -p 'require("./scripts/getver.js").default("full")')
