5863211701b4322252ee1bc2bbb4e023758b9850
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-21 22:01:13 +1100

    Set stable patch ver transiently based on prev tag

--- scripts/transientbumpver.sh
+++ scripts/transientbumpver.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 printenv
-prevtag=$(git describe --tags --abbrev=0)
-echo ${prevtag}
+
 echo Current version: $(node -p 'require("./scripts/getver.js").default("full")')
+# Need to add something to force patch increment even if not merge from hotfix
+# (maybe a queue time variable for manual builds?)
 if [[ ("${CHANNEL}" == "stable") && ("${BUILD_SOURCEVERSIONMESSAGE}" =~ ^(Merge pull request \#[0-9]{1,4} from .*\/hotfix\/.*)|(Merge branch \'hotfix\/.*\')$) ]]
 then
-  node -e "require('./scripts/bumpver.js').default({'channel':'${CHANNEL}','bump':'patch'})"
+  prevtag=$(git describe --tags --abbrev=0)
+  echo ${prevtag}
+  patchver=$(npx semver -i patch "${prevtag}")
+  echo ${patchver}
+  # node -e "require('./scripts/bumpver.js').default({'channel':'${CHANNEL}','bump':'patch'})"
+  node -e "require('./scripts/setver.js').default({'channel':'${CHANNEL}','comp':'full','val':'${patchver}'})"
 
   echo Bump patch to: $(node -p 'require("./scripts/getver.js").default("full")')
 fi
