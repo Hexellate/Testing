2b08c89216b7bd03f214621e70d0995b58df081f
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-24 17:29:08 +1100

    Make debug update page less annoying

--- src/renderer/updater.js
+++ src/renderer/updater.js
@@ -2,6 +2,7 @@ import { ipcRenderer } from "electron";
 import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./app.scss";
+import "./updater.scss";
 
 export default class App extends Component {
   constructor(props) {
@@ -54,27 +55,20 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <div id="messagebody">
           <pre>{JSON.stringify(this.state.versionDetails, null, 2)}</pre>
         </div>
-        <button type="button" onClick={this.goToApp}>
-          Go To App
-        </button>
-        {/* <input
-          type="text"
-          name="channelBox"
-          value={this.state.channelBox}
-          onChange={evt => this.handleInputChange(evt)}
-        />
-        <button type="button" onClick={() => this.setChannel()}>
-          Set Channel
-        </button> */}
-        <button type="button" onClick={() => this.checkUpdates()}>
-          Check For Updates
-        </button>
-        <button type="button" onClick={() => this.startUpdates()}>
-          Get Updates
-        </button>
+        <div id="buttonbar">
+          <button type="button" onClick={this.goToApp}>
+            Go To App
+          </button>
+          <button type="button" onClick={() => this.checkUpdates()}>
+            Check For Updates
+          </button>
+          <button type="button" onClick={() => this.startUpdates()}>
+            Get Updates
+          </button>
+        </div>
       </div>
     );
   }
