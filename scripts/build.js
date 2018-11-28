8f6183912e282b86bb222669142a8c8efc0f5374
Daniel Nichols <dn.danielnichols@gmail.com>
2018-11-29 03:13:39 +1100

    Add method to prevent restore after build and apply for CI server

--- scripts/build.js
+++ scripts/build.js
@@ -17,21 +17,22 @@ alts.set("l", ["l", "lin", "linux"]);
 
 // Parameters object to be used for prebuild and build
 const params = {
-  "platform": "",
-  "channel": "",
-  "pretag": "v0.5.0",
-  "projectName": "",
-  "projectOwner": "",
-  "sourceMessage": "",
-  "forcepatch": false,
-  "buildID": 0
+  platform: "",
+  channel: "",
+  pretag: "v0.5.0",
+  projectName: "",
+  projectOwner: "",
+  sourceMessage: "",
+  forcepatch: false,
+  norestore: false,
+  buildID: 0
 };
 /**
  * Validates a channel input, and exits program if it cannot be processed
  * @param  {string} rawChannel - The raw input for channel parameter
  * @return {string} The calculated channel value
  */
-const procChannel = (rawChannel) => {
+const procChannel = rawChannel => {
   let channel = `${rawChannel}`;
   channel = channel.replace(/undefined/g, "").toLowerCase();
   for (const i in channels) {
@@ -55,7 +56,7 @@ const procChannel = (rawChannel) => {
  * @param  {string} rawPlatform - The raw input for platform parameter
  * @return {string} The calculated platform value
  */
-const procPlatform = (rawPlatform) => {
+const procPlatform = rawPlatform => {
   let platform = `${rawPlatform}`;
   platform = platform.replace(/undefined/g, "").toLowerCase();
   for (const i in platforms) {
@@ -97,17 +98,22 @@ const procPlatform = (rawPlatform) => {
  */
 const procEnv = () => {
   const env = {
-    "projectName": "projectName",
-    "projectOwner": "projectOwner",
-    "sourceMessage": "foobar",
-    "buildID": 270,
-    "forcepatch": false
+    projectName: "projectName",
+    projectOwner: "projectOwner",
+    sourceMessage: "foobar",
+    buildID: 270,
+    forcepatch: false
   };
-  if (process.env.git_project_name !== undefined) env.projectName = process.env.git_project_name;
-  if (process.env.git_project_owner !== undefined) env.projectOwner = process.env.git_project_owner;
-  if (process.env.BUILD_SOURCEVERSIONMESSAGE !== undefined) env.sourceMessage = process.env.BUILD_SOURCEVERSIONMESSAGE;
-  if (process.env["Build.BuildId"] !== undefined) env.buildID = process.env["Build.BuildId"];
-  if (process.env.FORCEPATCH !== undefined) env.forcepatch = process.env.FORCEPATCH;
+  if (process.env.git_project_name !== undefined)
+    env.projectName = process.env.git_project_name;
+  if (process.env.git_project_owner !== undefined)
+    env.projectOwner = process.env.git_project_owner;
+  if (process.env.BUILD_SOURCEVERSIONMESSAGE !== undefined)
+    env.sourceMessage = process.env.BUILD_SOURCEVERSIONMESSAGE;
+  if (process.env["Build.BuildId"] !== undefined)
+    env.buildID = process.env["Build.BuildId"];
+  if (process.env.FORCEPATCH !== undefined)
+    env.forcepatch = process.env.FORCEPATCH;
   return env;
 };
 const argv = minimist(process.argv.slice(2));
@@ -121,6 +127,7 @@ if (argv.help || argv.h || argv._.includes("help")) {
   --platform win - Can be win, mac or lin. No platform will default to current system
   --pretag v0.5.0 - The tag of the previous release. Defaults to v0.5.0
   --forcepatch - Flag will force build to be a patch
+  --norestore - Prevents package.json and build configs from being restored at end of build.
 
   NOTE: Do NOT modify package.json or any channel config files while this script is running, as any changes WILL be overridden on exit!
   `);
@@ -132,12 +139,14 @@ params.platform = procPlatform(argv.platform);
 if (argv.pretag != undefined) params.pretag = argv.pretag;
 Object.assign(params, procEnv());
 if (argv.forcepatch != undefined) params.forcepatch = argv.forcepatch; // Override env forcepatch if arg is used
+if (argv.norestore != undefined) params.norestore = argv.norestore;
 
 console.log(`Calculated parameters to be used:`);
 console.log(`Channel: ${params.channel}`);
 console.log(`Platform: ${params.platform}`);
 console.log(`pretag: ${params.pretag}`);
 console.log(`forcepatch: ${params.forcepatch}`);
+console.log(`forcepatch: ${params.norestore}`);
 console.log(`projectName: ${params.projectName}`);
 console.log(`projectOwner: ${params.projectOwner}`);
 console.log(`buildID: ${params.buildID}`);
@@ -152,15 +161,15 @@ console.log("Step: Running prebuild clean process");
 const preCleaner = childProcess.exec("npm run clean && npm run pack");
 pids.push(preCleaner.pid);
 
-preCleaner.stdout.on("data", (chunk) => {
+preCleaner.stdout.on("data", chunk => {
   console.log(chunk);
 });
 
-preCleaner.stderr.on("data", (chunk) => {
+preCleaner.stderr.on("data", chunk => {
   console.error(chunk);
 });
 
-preCleaner.on("close", (code) => {
+preCleaner.on("close", code => {
   // preCleaner.removeAllListeners();
   if (code !== 0) {
     console.error(
@@ -177,17 +186,23 @@ preCleaner.on("close", (code) => {
     }.json`
   );
   pids.push(builder.pid);
-  builder.stdout.on("data", (chunk) => {
+  builder.stdout.on("data", chunk => {
     console.log(chunk);
   });
-  builder.stderr.on("data", (chunk) => {
+  builder.stderr.on("data", chunk => {
     console.error(chunk);
   });
 });
 
 process.on("beforeExit", () => {
-  console.log(`Step: Build process is finished. Running postbuild task.`);
-  postbuild.default();
+  if (params.norestore) {
+    console.log(
+      `Step: Build process is finished, however "norestore" flag has been set. Postbuild task will not be run.`
+    );
+  } else {
+    console.log(`Step: Build process is finished. Running postbuild task.`);
+    postbuild.default();
+  }
 
   console.log("Process Exiting. Terminating subprocesses...");
   for (const i in pids) {
