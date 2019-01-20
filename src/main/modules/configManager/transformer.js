0cffcbf7e8f3116b9780c2dcbef755dcf0256113
Daniel Nichols <pnupy9@gmail.com>
2019-01-20 14:40:13 +1100

    Remove classes from transformer
    Beginning of refactor to remove unneeded classes

--- src/main/modules/configManager/transformer.js
+++ src/main/modules/configManager/transformer.js
@@ -4,80 +4,84 @@ import log4js from "log4js";
 import * as Transformers from "./transformers";
 
 const log = log4js.getLogger("config-transformers");
+const type = "main";
+const ajv = new Ajv({ "allErrors": true, "useDefaults": true });
 
 /**
- * Used for transforming and validating config objects
- */
-export default class ConfigTransformer {
-  /**
-   * Constructs a new transformer
-   */
-  constructor() {
-    this._type = "main";
-    this.ajv = new Ajv({ "allErrors": true });
-  }
-
-  /**
    * Returns the config schema for the given version
    * @param {number} version The schema version to get
    */
-  _getSchema(version) {
-    return Transformers.schemaMatrix[this._type][`v${version}`];
-  }
+function getSchema(version) {
+  return Transformers.schemaMatrix[type][`v${version}`];
+}
 
-  /**
+/**
    * @return {number} The latest available config version
    */
-  get latest() {
-    return _.max(Transformers.versions[this._type]);
-  }
+function getLatest() {
+  return _.max(Transformers.versions[type]);
+}
 
-  /**
-   * Transforms the given configuration to the latest version
-   * @param {object} config The config object to transform
-   */
-  transform(config) { // TODO: Implement checksum stuff
-    const cVer = config.configVersion;
-    let newConfig = config;
-    log.info(`Config is version ${cVer}, latest is ${this.latest}`);
-    if (cVer < this.latest) {
-      log.info("Converting config to latest version");
-      for (let i = cVer + 1; i <= this.latest; i++) {
-        newConfig = Transformers.converterMatrix[this._type][`v${i}`](newConfig);
-        log.info(`Config transformed from ${cVer} to ${cVer + 1}`);
-        log.debug(`New config:`);
-        log.debug(JSON.stringify(newConfig, null, 2));
-        const validated = this.validateSync(newConfig, i);
-        if (validated !== true) {
-          throw new SyntaxError(`Config file ${this._type} failed to validate after transformation from ${i - 1} to ${i}: ${JSON.stringify(validated, null, 2)}`);
-        }
-      }
-    }
-    return newConfig;
-  }
-
-  /**
+/**
    * Performs a non-mutating validation on the given config
    * @param {object} config The configuration object to validate
    * @param {number} version The schema version to validate against
    */
-  async validate(config, version) {
-    const test = await this.ajv.compileAsync(this._getSchema(version));
-    return test(config);
+export async function validate(config, version) {
+  const test = await ajv.compileAsync(getSchema(version));
+  if (test(config)) {
+    return true;
   }
+  return test.errors;
+}
 
-  /**
+/**
    * Performs a synchronous non-mutating validation on the given config
    * @param {object} config The configuration object to validate
    * @param {number} version The schema version to validate against
    */
-  validateSync(config, version) {
-    const test = this.ajv.compile(this._getSchema(version));
-    if (test(config)) {
-      return true;
-    }
-    return test.errors;
-
-    // return test(config);
+export function validateSync(config, version) {
+  const test = ajv.compile(getSchema(version));
+  if (test(config)) {
+    return true;
   }
+  return test.errors;
+}
+
+/**
+   * Transforms the given configuration to the latest version
+   * @param {object} config The config object to transform
+   */
+export function transformSync(config) { // TODO: Implement checksum stuff
+  const cVer = config.configVersion;
+  const latest = getLatest();
+  log.info(`Config is version ${cVer}, latest is ${latest}`);
+
+  if (cVer < latest) {
+    let newConfig = config;
+    log.info("Converting config to latest version");
+
+    for (let i = cVer + 1; i <= latest; i++) {
+      newConfig = Transformers.converterMatrix[type][`v${i}`](newConfig);
+
+      log.info(`Config transformed from ${cVer} to ${cVer + 1}`);
+      log.debug(`New config:`);
+      log.debug(JSON.stringify(newConfig, null, 2));
+
+      const validated = validateSync(newConfig, i);
+      if (validated !== true) {
+        throw new SyntaxError(`Config file ${type} failed to validate after transformation from ${i - 1} to ${i}: ${JSON.stringify(validated, null, 2)}`);
+      }
+    }
+    return newConfig;
+  }
+  const validated = validateSync(config, cVer);
+  if (validated !== true) {
+    throw new SyntaxError(`Config file ${type} failed to validate: ${JSON.stringify(validated, null, 2)}`);
+  } else {
+    log.info("Config file validated successfully.");
+  }
+  return config;
+
+  // return newConfig;
 }
--- src/main/modules/configManager/transformers/v1.js
+++ src/main/modules/configManager/transformers/v1.js
@@ -3,7 +3,11 @@
 function MainToV1(config) {
   const converted = { ...config };
   converted.configVersion = 1;
-  converted.body = {};
+  converted.body = {
+    "updates": {
+      "autoUpdate": false,
+    },
+  };
   converted.revised = new Date();
   return converted;
 }
@@ -11,23 +15,51 @@ function MainToV1(config) {
 
 const SchemaMainV1 = {
   "type": "object",
-  // "additionalProperties": false,
+  "default": {},
   "required": ["configVersion", "revised", "body"],
-  "items": {
+  "properties": {
     "configVersion": {
       "type": "number",
+      "default": 1,
     },
     "checksum": {
       "type": "string",
     },
     "revised": {
       "type": "string",
+      "default": new Date(),
     },
     "body": {
       "type": "object",
+      "default": {},
+      "required": ["updates"],
+      "properties": {
+        "updates": {
+          "type": "object",
+          "default": {},
+          "required": ["autoUpdate"],
+          "properties": {
+            "autoUpdate": {
+              "type": "boolean",
+              "default": false,
+            },
+          },
+        },
+        "appReserved": {// On config version increments, these settings will probably be reset (may track versioning to an extent for each subtree, allowing proper final transformers to be written). Once a new config version hits the next channel, this section should be empty.
+          "type": "object",
+          "default": {},
+          "description": "Config namespaces that are not part of an official spec yet, but are used anyway. Most common for canary builds.",
+        },
+        "addonReserved": {
+          "type": "object",
+          "default": {},
+          "description": "Config namespaces to be used for addons (when implemented). Names will be a hash of addon name and author, beyond that entirely up to discretion of addon developer",
+        },
+      },
     },
   },
 };
+// When I get to addons, each will probably actually have it's own config file, where author can provide schemas and transformers. Still use hash naming though...
 
 
 // Matrix exports
