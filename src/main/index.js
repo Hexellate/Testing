de6a24eeace7b612cdac62c74fd9c9976b7acb56
Daniel Nichols <pnupy9@gmail.com>
2019-01-21 01:55:24 +1100

    Some refactoring of main

--- src/main/index.js
+++ src/main/index.js
@@ -4,93 +4,84 @@ import log4js from "log4js";
 import Path from "path";
 
 
-// import WindowManager from "./modules/window-manager";
-// import UpdateManager from "./modules/update-manager";
-import MainConfig from "./modules/config-manager";
+import WindowManager from "./modules/window-manager";
+import UpdateManager from "./modules/update-manager";
+import ConfigManager from "./modules/config-manager";
 
+app.on("ready", () => {
+  const isDevelopment = process.env.NODE_ENV !== "production";
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+  // Set app name if unpackaged
+  if (!app.isPackaged) {
+    app.setName("Auto Update Test - dev");
+  }
+  app.setPath("userData", Path.join(app.getPath("appData"), app.getName()));
 
-// Set app name if unpackaged
-if (!app.isPackaged) {
-  app.setName("Auto Update Test - dev");
-}
-app.setPath("userData", Path.join(app.getPath("appData"), app.getName()));
+  app.setPath("logs", Path.join(app.getPath("userData"), "logs"));
 
-app.setPath("logs", Path.join(app.getPath("userData"), "logs"));
-
-// Create app file path
-// try {
-
-
-//   // Make dir userData
-//   try {
-//     fs.mkdirSync(app.getPath("userData", { recursive: true }));
-//   } catch (err) {
-//     console.error(`Unable to make directory "${app.getPath("userData")}":`);
-//     console.error(`${err.name}: ${err.message}`);
-//   }
-// } catch (err) {
-//   console.error("ERROR: Unable to create userData directory for app:");
-//   console.error(`${err.name}: ${err.message}`);
-//   console.error("Exiting...");
-//   app.exit(1);
-// }
-
-log4js.configure({
-  "appenders": {
-    "console": {
-      "type": "console",
+  // Configure logger
+  log4js.configure({
+    "appenders": {
+      "console": {
+        "type": "console",
+      },
+      "main": {
+        "type": "file",
+        "filename": `${Path.join(app.getPath("logs"), "main.log")}`,
+        "numBackups": 5,
+      },
+      "errorFile": {
+        "type": "file",
+        "filename": `${Path.join(app.getPath("logs"), "error.log")}`,
+      },
+      "errors": {
+        "type": "logLevelFilter",
+        "level": "ERROR",
+        "appender": "errorFile",
+      },
     },
-    "main": {
-      "type": "file",
-      "filename": `${Path.join(app.getPath("logs"), "main.log")}`,
-      "numBackups": 5,
+    "categories": {
+      "default": {
+        "appenders": [
+          "console",
+          "main",
+          "errors",
+        ],
+        "level": "debug",
+      },
     },
-    "errorFile": {
-      "type": "file",
-      "filename": `${Path.join(app.getPath("logs"), "error.log")}`,
-    },
-    "errors": {
-      "type": "logLevelFilter",
-      "level": "ERROR",
-      "appender": "errorFile",
-    },
-  },
-  "categories": {
-    "default": {
-      "appenders": [
-        "console",
-        "main",
-        "errors",
-      ],
-      "level": "debug",
-    },
-  },
+  });
+
+  // Init logger
+  const Log = log4js.getLogger("main");
+  autoUpdater.logger = log4js.getLogger("updater");
+
+  // Log startup info
+  Log.info(
+    `${
+      app.isPackaged
+        ? "App is packaged, likely a production environment."
+        : "App is NOT packaged! Using dev configuration"
+    }`
+  );
+  Log.info(`Begin log for application: ${app.getName()}`);
+  Log.info(`Working directory: ${app.getPath("userData")}`);
+  Log.info(`Logging to ${app.getPath("logs")}`);
+
+  // Initialization sequence
+  const config = ConfigManager.createManager(isDevelopment, "main");
+  const windowManager = WindowManager.createManager(isDevelopment, "main");
+  const updateManager = UpdateManager.createManager(isDevelopment, "main");
+  config.preinit().then(() => {
+    windowManager.init();
+  }).then(() => {
+    updateManager.init();
+  });
 });
 
-const Log = log4js.getLogger("main");
-autoUpdater.logger = log4js.getLogger("updater");
-
-Log.info(
-  `${
-    app.isPackaged
-      ? "App is packaged, likely a production environment."
-      : "App is NOT packaged! Using dev configuration"
-  }`
-);
-Log.info(`Begin log for application: ${app.getName()}`);
-Log.info(`Working directory: ${app.getPath("userData")}`);
-Log.info(`Logging to ${app.getPath("logs")}`);
-
-
-const configManager = new MainConfig(isDevelopment);
-configManager.preinit().then(() => {
-
-});
 /*
-const windowManager = new WindowManager(isDevelopment, configManager);
-const updateManager = new UpdateManager(isDevelopment, configManager, windowManager);
+
+
 configManager.init()
   .then(() => {
     windowManager.init();
